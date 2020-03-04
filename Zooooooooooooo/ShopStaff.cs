5ee413d7c04576050a934b365103cbf995ed785f
Ellie-Anne Watts <ellieanne.watts@gmail.com>
2020-03-04 15:26:01 +0000

    Staff Version 1.0 ZooStaff Version 1.0 ShopStaff version 1.0

---
+++ Zooooooooooooo/ShopStaff.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zooooooooooooo
+{
+    class ShopStaff : Staff
+    {
+        public ShopStaff(DateTime _workStart, DateTime _workFinish, int _salary, string _jobTitle, int _age) : base(_workStart, _workFinish, _salary, _jobTitle, _age)
+        {
+        }
+    }
+}
---
+++ Zooooooooooooo/Staff.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zooooooooooooo
+{
+    class Staff : Person
+    {
+        protected DateTime workStart;
+        protected DateTime workFinish;
+        protected int salary;
+        protected string jobTitle;
+
+        public Staff (DateTime _workStart, DateTime _workFinish, int _salary, string _jobTitle, int _age) : base(_age)
+        {
+            workStart = _workStart;
+            workFinish = _workFinish;
+            salary = _salary;
+            jobTitle = _jobTitle;
+        }
+    }
+}
