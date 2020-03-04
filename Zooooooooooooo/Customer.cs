08597ea6a8932b583091126fbfeef4907a70c89f
Ellie-Anne Watts <ellieanne.watts@gmail.com>
2020-03-04 13:10:27 +0000

    Added Person version 1.0 Added Customer version 1.0

---
+++ Zooooooooooooo/Customer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zooooooooooooo
+{
+    class Customer : Person
+    {
+        private string favouriteAnimal;
+        private int happiness;
+
+        public Customer(string _favouriteAnimal, int _happiness, int _age) : base(_age)
+        {
+            favouriteAnimal = _favouriteAnimal;
+            happiness = _happiness;
+        }
+    }
+}
---
+++ Zooooooooooooo/Person.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zooooooooooooo
+{
+    class Person
+    {
+        protected int age;
+
+        public Person (int _age)
+        {
+            age = _age;
+        }
+    }
+}
